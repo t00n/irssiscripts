d20e676119277f047d914ae34167f38d3aa6dac5
t00n <kelkununtel@hotmail.com>
2015-05-12 16:13:57 +0200

    [fix] removed useless print

--- excellent.pl
+++ excellent.pl
@@ -3,9 +3,6 @@ my @channels  = ("cacasurtondoigt");
 
 sub bootstrap {
     my ($msg, $server, $target) = @_;
-    Irssi:print($msg);
-    Irssi:print($server);
-    Irssi:print($target->{name});
     if ($target->{name} eq "#ulbba3info") {
         $server->command("MSG " . $target->{name}. " " . $msg . " excellent");
     }
